7837df3790044c1af010b948253d1aa28216c186
TAGARAM SAI KUSHAL <24r11a05l0@gcet.edu.in>
2026-01-09 21:16:17 +0530

    Implement CNN model for disease classification

---
+++ Milestone4/Frontend_server/Backend_server/text_disease_model/model.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+
+class MyCNNModel(nn.Module):
+    def __init__(self, num_classes):
+        super(MyCNNModel, self).__init__()
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+
+        self.fc_layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * 7 * 7, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.avgpool(x)
+        x = self.fc_layers(x)
+        return x
